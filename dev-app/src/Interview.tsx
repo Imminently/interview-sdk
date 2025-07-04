17b1d69720ac489c53d9f58d02c31d9422629c40
Alex Bennett <alex.bennett@imminently.co>
2025-07-04 18:31:50 +1000

    Updated dev app

--- dev-app/src/Interview.tsx
+++ dev-app/src/Interview.tsx
@@ -1,52 +1,59 @@
-import { ManagerOptions } from "@imminently/interview-sdk";
+import { ApiManager, AttributeValues, AuthConfig, ManagerOptions, Session, Simulate } from "@imminently/interview-sdk";
 import { Interview } from "@imminently/interview-ui";
 
 // place token here for testing
-const user = {};
+const user = {}
 
 export const API = {
   baseUrl: "https://api.dev.decisively.imminently.co",
   tenancy: "32b1f94f-ef39-4b8b-a49f-8b6f5f72fd8d",
-  /** aka project */
-  model: "b2063cf7-ac77-44d8-a2c8-522fa6eadefb",
-  release: "a934e45c-e91a-4f47-ac6a-ba74309c7467",
-  interview: "91a84a89-5e48-4158-a5d7-bc622cfe34cd",
-  // goal: "27c114ac-337e-4dbc-8c53-457588397cfb"
+  /** main brii project */
+  // model: "af7715f3-9242-4646-a614-0932b598c5c8",
+  // interview: "Award selection interview (embeds rate determination)",
+  // dev control test
+  model: "42d3e876-af7d-4579-b17f-514ee08487b8",
+  interview: "Control test",
+}
+
+class CustomApi extends ApiManager {
+  simulate = async (session: Session, data: Partial<Simulate>) => {
+    // Dynamic interactions are now on a post (due to new interaction behaviour in backend)
+    const res = await this.api.post<AttributeValues>(
+      buildUrl(session.sessionId, "interview"),
+      {
+        mode: "api",
+        save: false,
+        ...data,
+      }
+    );
+    return res.data;
+  }
+
+  getRulesEngineUrl = (checksum?: string) => {
+    return `${this.options.host}/rules-engine?=${checksum}`;
+  }
 }
 
 export const getInterviewConfig = (interview?: string) => {
   const token = user.id_token as string;
+  const auth: AuthConfig = {
+    token: `Bearer ${token}`,
+    tenancy: API.tenancy,
+  };
   return {
     debug: true,
-    host: API.baseUrl,
-    tenancyId: API.tenancy,
-    apiToken: token,
-    // api: API,
     sessionConfig: {
       project: API.model,
+      // release: "interview",
       interview: interview ?? API.interview,
-      // release: "0eea8a84-436d-4c8e-8745-14f345726f41",
-      release: API.release,
-      // sessionId: sessionId ?? undefined,
     },
+    apiManager: new CustomApi({
+      host: API.baseUrl,
+      // path: "timesheet",
+      auth,
+    }),
     fileManager: {
       host: API.baseUrl,
-    },
-    overrides: {
-      transformRequest: [
-        (data, headers) => {
-          if (headers) {
-            const tenancy = API.tenancy;
-
-            headers.Authorization = token
-              ? `Bearer ${token}`
-              : undefined;
-            headers["X-TENANCY"] = tenancy;
-          }
-
-          return JSON.stringify(data);
-        },
-      ],
     },
   } as ManagerOptions;
 }
@@ -56,4 +63,8 @@ export const InterviewPage = () => {
   return (
     <Interview options={options} />
   )
+}
+
+function buildUrl(model: string, release: string): string {
+  throw new Error("Function not implemented.");
 }
--- dev-app/src/main.tsx
+++ dev-app/src/main.tsx
@@ -37,7 +37,7 @@ const Wrapper = () => {
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Wrapper />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Wrapper />
+  // </React.StrictMode>
 );
