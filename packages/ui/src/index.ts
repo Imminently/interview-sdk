16e88833163caddfefcf1a811970ff42f6da4f1b
Alex Bennett <alex.bennett@imminently.co>
2025-08-25 16:14:17 +1000

    Explanation and translation fixes
    - Exporting setTranslateFn
    - Updated Explanation to useAttributeToFieldName as we need the correct id

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,6 @@ export * from "./interview";
 export * from "./components";
 // we only want to export certain providers
 export * from "./providers/ThemeProvider";
+export { setTranslateFn } from "./util/translate-fn";
 
 import "./styles.css"; // ensure styles are included
